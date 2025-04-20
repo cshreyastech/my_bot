561d9aea33ecc813ee13eeec5a95c335f533d5b2
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2025-04-20 10:19:30 -0700

    Working on setting up gazebo.

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  package_name = 'my_bot'
+  rsp = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([os.path.join(
+      get_package_share_directory(
+        package_name), 'launch', 'rsp.launch.py'
+    )]), launch_arguments={'use_sim_time': 'true'}.items()
+  )
+
+  joint_state_publisher_gui = Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+            )
+  rviz2 = Node(
+          package='rviz2',
+          executable='rviz2',
+          name='rviz2',
+          output='screen',
+          arguments=['-d', 'src/my_bot/config/my_bot.rviz']
+          )
+
+  return LaunchDescription([
+    rsp,
+    joint_state_publisher_gui,
+    rviz2
+  ])
