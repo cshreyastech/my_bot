d29e8e689b75106330def7e958d42107eabfe134
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2025-05-02 04:43:29 -0700

    Completed How do we add LIDAR to a ROS robot?

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,9 +1,15 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
+
 
 
 def generate_launch_description():
@@ -31,8 +37,66 @@ def generate_launch_description():
     arguments=['-d', 'src/my_bot/config/my_bot_v2.rviz']
   )
 
+  default_world = os.path.join(
+    get_package_share_directory(package_name),
+    'worlds',
+    'obstacles.world'
+    )
+  
+  world = LaunchConfiguration('world')
+
+  world_arg = DeclareLaunchArgument(
+    'world',
+    default_value=default_world,
+    description='World to load'
+    )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+  gazebo = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([os.path.join(
+    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+
+  # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+  spawn_entity = Node(package='ros_gz_sim', executable='create',
+    arguments=['-topic', 'robot_description',
+    '-name', 'my_bot',
+    '-z', '0.1'],
+    output='screen')
+
+  # diff_drive_spawner = Node(
+  #   package="controller_manager",
+  #   executable="spawner",
+  #   arguments=["diff_cont"],
+  # )
+
+  # joint_broad_spawner = Node(
+  #   package="controller_manager",
+  #   executable="spawner",
+  #   arguments=["joint_broad"],
+  # )
+
+  bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+  
+  ros_gz_bridge = Node(
+    package="ros_gz_bridge",
+    executable="parameter_bridge",
+    arguments=[
+      '--ros-args',
+      '-p',
+      f'config_file:={bridge_params}',
+    ]
+  )
+
   return LaunchDescription([
     rsp,
     # joint_state_publisher_gui,
-    rviz2
+    rviz2,
+    world_arg,
+    gazebo,
+    spawn_entity,
+    # diff_drive_spawner,
+    # joint_broad_spawner,
+    ros_gz_bridge
   ])
