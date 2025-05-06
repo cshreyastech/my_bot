103575cecf0f0cacfb9d9185ff6442c9b8a867a0
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2025-05-06 05:21:54 -0700

    10-How to get your robot to see in 3D!

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -40,7 +40,7 @@ def generate_launch_description():
   default_world = os.path.join(
     get_package_share_directory(package_name),
     'worlds',
-    'obstacles.world'
+    'obstacles.sdf'
     )
   
   world = LaunchConfiguration('world')
